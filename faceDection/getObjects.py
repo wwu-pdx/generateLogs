fbb41853999e3145f013539a4ca9ee1212190194
Wenjing Wu <wwu@pdx.edu>
2022-01-03 15:56:55 -0800

    add new files

---
+++ faceDection/getObjects.py
@@ -0,0 +1,5 @@
+from filelist import filelist
+
+filenames=filelist
+filelist.pop(1)
+print(filelist)
--- faceDection/outputFilenames.py
+++ faceDection/outputFilenames.py
@@ -1,18 +1,11 @@
 import requests
-import os
 import sys
+import os
+filenames= []
 count = 0
-
 num = int(sys.argv[1]) #number of files want to use
-if len(sys.argv) <3:
-   url = 'https://us-central1-lognlp.cloudfunctions.net/pd5-f-access-713917766553'
-else:
-   url = sys.argv[2]
 #directory = r'G:\Face_dataset\original_images\original_images'
-headers={
-'Referer' :'http://us-central1-lognlp.cloudfunctions.net/',
-'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-}
+
 directory = r'original_images'
 for entry in os.scandir(directory):
      if (entry.path.endswith(".jpg")
@@ -20,15 +13,16 @@ for entry in os.scandir(directory):
          #print(entry.path)
          with open(entry.path, 'rb') as img:
             name_img= os.path.basename(entry.path)
-            files= {'file': (name_img,img,'multipart/form-data',{'Expires': '0'}) } 
-            
-            
+            filenames.append(name_img)
+            print(name_img)
             #response=requests.post(url, files=files,headers=headers)
             #print(response.status_code)
             #print(response.text)
          count+=1
          
 print(count)
-print(url)
-
+#print(filenames)
+f = open("filelist.py", "w")
+f.write(f'filelist={filenames}')
+f.close()
 
