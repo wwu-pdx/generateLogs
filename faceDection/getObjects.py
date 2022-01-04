3c8a9e7dd8de8d2b8f6b8bf67a6303fce28902e8
Wenjing Wu <wwu@pdx.edu>
2022-01-03 23:09:12 -0800

    edit json path

--- faceDection/getObjects.py
+++ faceDection/getObjects.py
@@ -1,5 +1,22 @@
 from filelist import filelist
+from google.cloud import storage
+from googleapiclient import discovery
+import google.auth
+import sys
+import random
 
 filenames=filelist
-filelist.pop(1)
-print(filelist)
+num = int(sys.argv[1])
+
+intervel = int(len(filenames)/num)
+res = random.sample(range(1, intervel), num)
+
+#storage_client = storage.Client()
+storage_client = storage.Client.from_service_account_json('pd5-access.json')
+buckets = storage_client.list_buckets()
+bucketname=''
+for bucket in buckets:
+    if bucket.name.startswith( 'pd5-bucket-' ):
+        bucketname= bucket.name
+print(res)
+
